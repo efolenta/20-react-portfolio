622dd3f1a71f41c40f34f8c62b68f8f84d4c95ab
Eric Folenta <ericfolenta@gmail.com>
2020-11-12 17:17:23 -0800

    updated nav theme

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <Link className="navbar-brand" to="#">Navbar</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
