ab36b52eb3348b525b45e6f21a521d05b2fce99d
Eric Folenta <ericfolenta@gmail.com>
2020-11-12 17:14:04 -0800

    added jumbotron component

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4">Eric Folenta</h1>
+                <p className="lead">Full Stack Web Developer from UC Irvine</p>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Jumbotron;
