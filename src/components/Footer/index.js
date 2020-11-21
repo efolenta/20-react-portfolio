3885e9ea0af30505e7c44c89a0316396acdfd70c
Eric Folenta <ericfolenta@gmail.com>
2020-11-20 17:33:40 -0800

    added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Col } from "../Grid";
+
+function Footer() {
+    return (
+        <>
+        <Col size="md-8">
+            <h1>Eric Folenta Portfilo</h1>
+            <p><a href="https://github.com/efolenta">View Github</a></p>
+            <p><a href="https://www.linkedin.com/in/eric-folenta/">View LinkedIn</a></p>
+        </Col>
+        <Col size="md-4">
+            <p align="right">Copyright 2020</p>
+        </Col>
+        </>
+    )
+}
+
+export default Footer;
