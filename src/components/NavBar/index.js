9f60d5ba459b3e367ca3853fd1f74f2d8db69580
Eric Folenta <ericfolenta@gmail.com>
2020-11-21 18:40:59 -0800

    tweaked navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,23 +3,17 @@ import { Link } from "react-router-dom";
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="#">Navbar</Link>
+        <Link className="navbar-brand" to="/">Eric Folenta</Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-            <li className="nav-item active">
-                <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
+            <li className="nav-item">
+                <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="#">Features</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="#">Pricing</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link disabled" to="#">Disabled</Link>
+                <Link className="nav-link" to="/portfolio">Portfolio</Link>
             </li>
             </ul>
         </div>
