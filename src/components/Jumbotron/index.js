414b2dc60c66db8049db4638a87bf31c1d52a1fe
Eric Folenta <ericfolenta@gmail.com>
2020-11-21 18:40:30 -0800

    updated framing of elements

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Row } from "../Grid";
 
-function Jumbotron() {
+function Jumbotron(props) {
     return (
         <div className="jumbotron-fluid">
             <div className="container">
-                <h1 className="display-4">Eric Folenta</h1>
+                <Row>
+                <div className="col-md-6">
+                <h1 className="display-4">{props.title}</h1>
                 <p className="lead">Full Stack Web Developer from UC Irvine</p>
+                </div>
+                <div className="col-md-6" id="jumbotron-image">
+                <img src={props.image} alt="computer" id={props.class} />
+                </div>
+                </Row>
             </div>
         </div>
         
