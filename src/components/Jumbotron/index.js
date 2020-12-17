4ee100703a7fd4b402a8702a66db1225ca49cd49
Eric Folenta <ericfolenta@gmail.com>
2020-12-16 20:54:40 -0800

    replaced static text with prop

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -8,7 +8,7 @@ function Jumbotron(props) {
                 <Row>
                 <div className="col-md-6">
                 <h1 className="display-4">{props.title}</h1>
-                <p className="lead">Full Stack Web Developer from UC Irvine</p>
+                <p className="lead">{props.subtext}</p>
                 </div>
                 <div className="col-md-6" id="jumbotron-image">
                 <img src={props.image} alt="computer" id={props.class} />
