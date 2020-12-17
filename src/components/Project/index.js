f8089a5610c67e594ee36a9fe1a8ff6b7f8bbc1a
Eric Folenta <ericfolenta@gmail.com>
2020-12-16 20:42:21 -0800

    added github logo to card

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import githubMark from "../../assets/GitHub-Mark-Light-64px.png";
 
 function Project(props) {
     return (
@@ -12,7 +13,7 @@ function Project(props) {
                     <p className="card-text">{props.body}</p>
                 </div>
                 <div className="card-footer" id="github-url">
-                    <p><a href={props.url}>View Source</a></p>
+                    <p><a href={props.url}>View Source <img width="32px" src={githubMark} alt="GitHub Logo"/></a></p>
                 </div>
             </div>
         </>
