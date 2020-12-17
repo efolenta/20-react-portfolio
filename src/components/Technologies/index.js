a2b8cf779c57ff47a2131c2d08cc0ccca6c78394
Eric Folenta <ericfolenta@gmail.com>
2020-12-16 21:25:12 -0800

    Created framework for tech component

---
+++ src/components/Technologies/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Col, Container, Row } from "../Grid";
+
+function Technologies () {
+    return (
+        <>
+        <Row>
+            <Col size="md-2 xs-3">
+                <p>HTML</p>
+            </Col>
+            <Col size="md-2 xs-3">
+                <p>CSS</p>
+            </Col>
+            <Col size="md-2 xs-3">
+                <p>CSS</p>
+            </Col>
+            <Col size="md-2 xs-3">
+                <p>Node.js</p>
+            </Col>
+            <Col size="md-2 xs-3">
+                <p>MySQL</p>
+            </Col>
+            <Col size="md-2 xs-3">
+                <p>MongoDB</p>
+            </Col>
+        </Row>
+        <Row>
+            <Col size="md-2 xs-4">
+                <p>React</p>
+            </Col>
+        </Row>
+        </>
+    )
+}
+
+export default Technologies;
