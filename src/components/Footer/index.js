bbafc27671397263d79f8bc861348a54febddc15
Eric Folenta <ericfolenta@gmail.com>
2021-01-17 08:31:03 -0800

    Removed the copyright and changed grid structure

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,13 +4,12 @@ import { Col } from "../Grid";
 function Footer() {
     return (
         <>
-        <Col size="md-8">
+        <Col size="md-6">
             <h1>Eric Folenta Portfilo</h1>
+        </Col>
+        <Col size="md-6">
             <p><a href="https://github.com/efolenta">View Github</a></p>
             <p><a href="https://www.linkedin.com/in/eric-folenta/">View LinkedIn</a></p>
-        </Col>
-        <Col size="md-4">
-            <p align="right">Copyright 2021</p>
         </Col>
         </>
     )
