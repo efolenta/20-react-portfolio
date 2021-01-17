758030c5cabde5753f03ef1e277c71635b1bbd16
Eric Folenta <ericfolenta@gmail.com>
2021-01-17 08:30:36 -0800

    Created new copyright component

---
+++ src/components/Copyright/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Col } from "../Grid";
+
+function Copyright() {
+    return (
+        <>
+        <Col size="md-12">
+            <p align="center">Copyright 2021</p>
+        </Col>
+        </>
+    )
+}
+
+export default Copyright;
