f9b9db995b3e9a902f288bd3f759e94151fb1b38
Eric Folenta <ericfolenta@gmail.com>
2021-01-19 19:02:44 -0800

    Tweaked size and added button

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -6,13 +6,14 @@ function Jumbotron(props) {
         <div className="jumbotron-fluid">
             <div className="container">
                 <Row>
-                <div className="col-md-6">
-                <h1 className="display-4">{props.title}</h1>
-                <p className="lead">{props.subtext}</p>
-                </div>
-                <div className="col-md-6" id="jumbotron-image">
-                <img src={props.image} alt="computer" id={props.class} />
-                </div>
+                    <div className="col-md-6">
+                        <h1 className="display-3">{props.title}</h1>
+                        <p className="lead">{props.subtext}</p>
+                        <button type="button" class="btn btn-primary">Primary</button>
+                    </div>
+                    <div className="col-md-6" id="jumbotron-image">
+                        <img src={props.image} alt="computer" id={props.class} />
+                    </div>
                 </Row>
             </div>
         </div>
