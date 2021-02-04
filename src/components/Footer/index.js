b1e1a70e925bedf94653f4bead6b3f1faab6b0cc
Eric Folenta <ericfolenta@gmail.com>
2021-02-04 08:46:05 -0500

    Updated Grid and Footer content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,16 +1,23 @@
 import React from "react";
-import { Col } from "../Grid";
+import Copyright from "../Copyright";
+import { Col, Row } from "../Grid";
 
 function Footer() {
     return (
         <>
-        <Col size="md-6">
-            <h1>Eric Folenta</h1>
-        </Col>
-        <Col size="md-6">
-            <p><a href="https://github.com/efolenta">View Github</a></p>
-            <p><a href="https://www.linkedin.com/in/eric-folenta/">View LinkedIn</a></p>
-        </Col>
+        <Row>
+            <Col size="md-6">
+                <h1>Let's Work Together</h1>
+            </Col>
+            <Col size="md-6">
+                <p></p>
+                <p><a href="https://github.com/efolenta">See my Github</a></p>
+                <p><a href="https://www.linkedin.com/in/eric-folenta/">Connect on LinkedIn</a></p>
+            </Col>
+        </Row>
+        <Row>
+            <Copyright />
+        </Row>
         </>
     )
 }
