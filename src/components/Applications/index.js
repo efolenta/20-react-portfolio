fa583fcc6aeaa6b3dab86ff1fa6674e6ff83e3f3
Eric Folenta <ericfolenta@gmail.com>
2021-02-04 08:29:08 -0500

    Added new component

---
+++ src/components/Applications/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Col, Row } from "../Grid";
+
+function Applications() {
+    return (
+    
+        <div align="center">
+            <h1 align="center">Software and Applications</h1>
+            <hr />
+        </div>
+
+    )
+}
+
+export default Applications;
