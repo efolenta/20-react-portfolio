781e75ee685e92f167459f08817b2260714f2d7f
Eric Folenta <ericfolenta@gmail.com>
2021-02-04 20:19:10 -0500

    refactored code

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,15 +9,15 @@ function NavBar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/portfolio">Portfolio</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/about">About</Link>
-            </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/portfolio">Portfolio</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link" to="/about">About</Link>
+                </li>
             </ul>
         </div>
         </nav>
